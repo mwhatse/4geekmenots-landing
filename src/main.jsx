2ca822ad027c4d53a0a73f6aa120adf71f898539
Trebor Jones <4geekmenots@gmail.com>
2025-10-30 01:05:02 -0400

    added a comment for main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
-// Mount React 18 to #root (defined in index.html)
-const rootEl = document.getElementById("root");
-if (!rootEl) {
-  throw new Error("Root element #root not found");
-}
-createRoot(rootEl).render(<App />);
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
